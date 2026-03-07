a4848ec5f406485a4b8c3979ef671b54304af305
a-wing <1@233.email>
2026-03-07 16:42:06 +0800

    fix cargo clippy && cargo update

--- liveion/src/route/admin.rs
+++ liveion/src/route/admin.rs
@@ -25,10 +25,10 @@ async fn cascade(
             "src and dst cannot be non-empty at the same time",
         ));
     }
-    if body.source_url.is_some() {
+    if let Some(source_url) = body.source_url {
         state
             .stream_manager
-            .cascade_pull(stream, body.source_url.unwrap(), body.token)
+            .cascade_pull(stream, source_url, body.token)
             .await?;
     } else {
         state
